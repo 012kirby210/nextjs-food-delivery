b6272f63500728561c42edfc922651fa488b68cb
012kirby210 <012kirby210@gmail.com>
2023-12-25 08:26:11 +0100

    Ajout Provider apollo.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import '@/styles/globals.css';
 import Layout from '@/components/Layout.js';
 
+const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
+
+export const client = new ApolloClient({
+  uri: `${STRAPI_URL}/graphql`,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    mutate: {
+      errorPolicy: "all",
+    },
+    query: {
+      errorPolicy: "all",
+    },
+  },
+});
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <ApolloProvider client={client}>
+      <Layout>
       <Component {...pageProps} />
     </Layout>
+    </ApolloProvider>
   );
 }
