919fce24aaa2cd4fafc62ea15c1c74bc09e150a3
012kirby210 <012kirby210@gmail.com>
2024-05-26 06:53:56 +0200

    Ajout pattern d'accès aux images dans  la configuration nextjs.

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         hostname: "localhost",
         port: "1337",
         pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
       }
     ]
   }
